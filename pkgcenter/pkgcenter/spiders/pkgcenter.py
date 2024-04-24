8ef5c2b7f6d1669f57561130a8749ebbb19ead84
Kamran Anwar <101544136+Kamran6789@users.noreply.github.com>
2024-04-24 19:09:10 +0500

    Update pkgcenter.py

--- pkgcenter/pkgcenter/spiders/pkgcenter.py
+++ pkgcenter/pkgcenter/spiders/pkgcenter.py
@@ -18,13 +18,10 @@ class PkgcenterSpider(scrapy.Spider):
         items['3rd place business'] = response.css("p+ ul ::text").extract()[9].replace('\xa0', '').replace('(', '').replace(')', '')
         items['4th place business'] = response.css("p+ ul ::text").extract()[12]
 
-        table.create(items)
+        yield(items)
 
 
 if __name__ == '__main__':
-    api = pyairtable.Api('patAutBB8czI3ifML.c1e5aa28bb6f09cbb0d81815494d371003902e6f43500abebe713f307a505f25')
-    base = api.base('appISH2KhnZt5ElD8')
-    table = [v for v in base.tables() if v.name == 'Main Table'][0]
     process = CrawlerProcess()
     process.crawl(PkgcenterSpider)
     process.start()
